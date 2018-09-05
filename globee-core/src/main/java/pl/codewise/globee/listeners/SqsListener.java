e7c7d012131c5a58ac1e8e9de12dbb652dd68393
stanislaw-tokarski <stanislaw.tokarski@codewise.com>
2018-09-05 12:18:13 +0200

    Improved retries mechanism by using ScheduledExecutorService

--- globee-core/src/main/java/pl/codewise/globee/listeners/SqsListener.java
+++ globee-core/src/main/java/pl/codewise/globee/listeners/SqsListener.java
@@ -1,26 +1,45 @@
 package pl.codewise.globee.listeners;
 
 import com.amazonaws.services.autoscaling.model.AmazonAutoScalingException;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.codewise.globee.exceptions.UnsupportedMessageFormReceivedException;
 import pl.codewise.globee.services.caching.ResourcesStorage;
 import pl.codewise.globee.services.caching.SqsMessagesDeserializer;
 
+import javax.annotation.PreDestroy;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@AllArgsConstructor
 @Service
 public class SqsListener implements MessageListener {
 
     private final ResourcesStorage resourcesStorage;
     private final SqsMessagesDeserializer sqsMessagesDeserializer;
+    private final int maxRetriesNumber;
+    private final int timeoutBetweenRetries;
+    private final ScheduledExecutorService unprocessedExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    public SqsListener(ResourcesStorage resourcesStorage,
+            SqsMessagesDeserializer sqsMessagesDeserializer, @Value("${max.retries.number}") int maxRetriesNumber,
+            @Value("${timeout.between.retries}") int timeoutBetweenRetries) {
+        this.resourcesStorage = resourcesStorage;
+        this.sqsMessagesDeserializer = sqsMessagesDeserializer;
+        this.maxRetriesNumber = maxRetriesNumber;
+        this.timeoutBetweenRetries = timeoutBetweenRetries;
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        unprocessedExecutorService.shutdown();
+    }
 
     @Override
     public void onMessage(Message message) {
@@ -28,30 +47,37 @@ public class SqsListener implements MessageListener {
     }
 
     private void handleReceivedMessage(Message message) {
-        boolean messageProcessed = processMessage((TextMessage) message);
-        if (messageProcessed) {
-            try {
-                message.acknowledge();
-            } catch (JMSException e) {
-                log.warn("Unable to acknowledge message: {}", e.getMessage());
-            }
-        } else {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-                handleReceivedMessage(message);
-            } catch (InterruptedException e) {
-                log.warn("Unable to sleep thread: {}", e.getMessage());
-            }
+        final String messagePayload;
+        try {
+            messagePayload = ((TextMessage) message).getText();
+            message.acknowledge();
+        } catch (JMSException e) {
+            log.error("Unable to read message from SQS", e);
+            return;
+        }
+        if (!processMessage(messagePayload)) {
+            tryProcessInFuture(messagePayload, maxRetriesNumber);
         }
     }
 
-    private boolean processMessage(TextMessage message) {
-        String messageText = "<message text expected here>";
+    private void tryProcessInFuture(String messagePayload, Integer tryNumber) {
+        Runnable process = () -> {
+            if (tryNumber == 0) {
+                log.error("Max number of retries reached, message will not be processed");
+                return;
+            }
+            if (!processMessage(messagePayload)) {
+                tryProcessInFuture(messagePayload, tryNumber - 1);
+            }
+        };
+        unprocessedExecutorService.schedule(process, timeoutBetweenRetries, TimeUnit.SECONDS);
+    }
+
+    private boolean processMessage(String messageText) {
         try {
-            messageText = message.getText();
             sqsMessagesDeserializer.readMessage(messageText).forEach(r -> r.visit(resourcesStorage));
-        } catch (UnsupportedMessageFormReceivedException | JMSException e) {
-            log.warn("Unable to handle message received from SQS: \n{}", messageText, e);
+        } catch (UnsupportedMessageFormReceivedException e) {
+            log.error("Unable to handle message received from SQS:\n{}", messageText, e);
         } catch (AmazonAutoScalingException e) {
             log.warn("Rate exceeded while updating data, retry in one second: {}", e.getErrorMessage());
             return false;
