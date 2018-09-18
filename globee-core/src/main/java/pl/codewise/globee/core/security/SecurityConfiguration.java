9e7ef2bce592f7eaa2837743ed37ca368bc0633a
stanislaw-tokarski <stanislaw.tokarski@codewise.com>
2018-09-18 14:53:35 +0200

    SecurityConfiguration fix

--- globee-core/src/main/java/pl/codewise/globee/core/security/SecurityConfiguration.java
+++ globee-core/src/main/java/pl/codewise/globee/core/security/SecurityConfiguration.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Configuration
 @ComponentScan("pl.codewise.globee.core.security")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
