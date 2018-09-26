7a1714d77a6c165497e4d189d7b5604a9cc7c39b
stanislaw-tokarski <stanislaw.tokarski@codewise.com>
2018-09-26 16:48:39 +0200

    CORS fix

--- globee-core/src/main/java/pl/codewise/globee/core/security/SecurityConfiguration.java
+++ globee-core/src/main/java/pl/codewise/globee/core/security/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package pl.codewise.globee.core.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @ComponentScan
@@ -14,6 +19,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     public SecurityConfiguration(FixedTokenAuthenticationFilter fixedTokenAuthenticationFilter) {
         this.fixedTokenAuthenticationFilter = fixedTokenAuthenticationFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 
     @Override
